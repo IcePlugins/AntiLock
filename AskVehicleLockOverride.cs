359cdf605ea98f787a8d0435d279589f169589a5
Sl4vP0weR <42337892+Sl4vP0weR@users.noreply.github.com>
2021-06-17 02:52:58 +0800

    Refactored, updated, cleaned.

---
+++ AskVehicleLockOverride.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System.Linq;
+using UnityEngine;
+using static AntiLock.Main;
+
+namespace AntiLock
+{
+    [HarmonyPatch(typeof(VehicleManager), nameof(VehicleManager.ReceiveVehicleLockRequest))]
+    static class AskVehicleLockOverride
+    {
+        // for debug
+        static bool Check(ServerInvocationContext context)
+        {
+            var steamID = context.GetCallingPlayer().playerID.steamID;
+            var up = UnturnedPlayer.FromCSteamID(steamID);
+            if (up.IsAdmin && conf.IgnoreAdmins)
+                return true;
+
+            var vehicle = up.Player.movement.getVehicle();
+
+            if (!vehicle?.checkDriver(steamID) ?? true) // not in the vehicle or aren't a driver
+                return false;
+
+            if (vehicle.isLocked)
+                return true;
+
+            var group = conf.LockGroups
+                .Where(x => up.HasPermission(x.Permission))
+                .OrderBy(x => x.MaxLocks)
+                .LastOrDefault();
+
+            int max = group == null ? conf.DefaultMaxLocks : group.MaxLocks;
+            int count = VehicleManager.vehicles.Count(x => x.lockedOwner == steamID && x.isLocked);
+
+            var maxLocks = count >= max && conf.DisplayMaxlocksNotice;
+            var msg = maxLocks ? inst.Translate(MaxLockedNotice, max) :
+                    (conf.DisplayLockNotice ? inst.Translate(LockedNotice, max - count - 1, max) : null);
+            var color = maxLocks ? Color.red : Color.green;
+
+            UnturnedChat.Say(up, msg, color);
+            return !maxLocks;
+        }
+        static bool Prefix(in ServerInvocationContext context) => Check(context);
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,42 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace AntiLock
+{
+    public class LockGroup
+    {
+        [XmlAttribute]
+        public string Permission { get; set; }
+        [XmlAttribute]
+        public int MaxLocks { get; set; }
+    }
+    public class Config : IRocketPluginConfiguration
+    {
+        public int DefaultMaxLocks = 0;
+        public bool 
+            IgnoreAdmins = true,
+            DisplayMaxlocksNotice = true,
+            DisplayLockNotice = true;
+        public string
+            AllowOtherPermission = "antilock.clearlocks.other";
+        public List<LockGroup> LockGroups = new List<LockGroup>();
+
+        public void LoadDefaults()
+        {
+            LockGroups = new List<LockGroup>
+            {
+                new LockGroup
+                {
+                    MaxLocks = 5,
+                    Permission = "antilock.lock"
+                },
+                new LockGroup
+                {
+                    MaxLocks = 10,
+                    Permission = "antilock.biglock"
+                }
+            };
+        }
+    }
+}
